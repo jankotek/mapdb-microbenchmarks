73ea6ce5999baa53c5d5fd8cbb28e3820688a137
Jan Kotek <jan@kotek.net>
2015-08-16 10:17:05 +0200

    Add byte[] hashing

---
+++ src/main/java/org/mapdb/jmh/ArrayHash.java
@@ -0,0 +1,57 @@
+package org.mapdb.jmh;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@State(Scope.Thread)
+@Warmup(iterations = 2)
+@Measurement(iterations = 5)
+public class ArrayHash {
+
+    static final byte[] b = new byte[16];
+    static{
+        new Random().nextBytes(b);
+    }
+
+
+    @Benchmark public int arrays(){
+        return Arrays.hashCode(b);
+    }
+
+    @Benchmark public int arrays2(){
+        int result = 1;
+        for (byte element : b)
+            result = 31 * result + element;
+
+        return result;
+    }
+
+    @Benchmark public int arrays257(){
+        int result = 1;
+        for (byte element : b)
+            result = 257 * result + element;
+
+        return result;
+    }
+
+    @Benchmark public int arraysB(){
+        int result = 1;
+        for (byte element : b)
+            result = -1640531527 * result + element;
+
+        return result;
+    }
+
+
+
+    @Benchmark public int arraysXXX(){
+        return XXHash32JavaUnsafe.hash(b,0,b.length,0);
+    }
+
+
+    @Benchmark public int arraysXXXSafe(){
+        return XXHash32JavaSafe.hash(b,0,b.length,0);
+    }
+}
---
+++ src/main/java/org/mapdb/jmh/XXHash32JavaSafe.java
@@ -0,0 +1,82 @@
+// Auto-generated: DO NOT EDIT
+
+package org.mapdb.jmh;
+
+import net.jpountz.util.SafeUtils;
+import net.jpountz.xxhash.XXHash32;
+
+import static java.lang.Integer.rotateLeft;
+import static org.mapdb.jmh.XXHashConstants.*;
+
+/**
+ * {@link XXHash32} implementation.
+ */
+final class XXHash32JavaSafe {
+
+  public static int hash(byte[] buf, int off, int len, int seed) {
+
+    SafeUtils.checkRange(buf, off, len);
+
+    final int end = off + len;
+    int h32;
+
+    if (len >= 16) {
+      final int limit = end - 16;
+      int v1 = seed + PRIME1 + PRIME2;
+      int v2 = seed + PRIME2;
+      int v3 = seed + 0;
+      int v4 = seed - PRIME1;
+      do {
+        v1 += SafeUtils.readIntLE(buf, off) * PRIME2;
+        v1 = rotateLeft(v1, 13);
+        v1 *= PRIME1;
+        off += 4;
+
+        v2 += SafeUtils.readIntLE(buf, off) * PRIME2;
+        v2 = rotateLeft(v2, 13);
+        v2 *= PRIME1;
+        off += 4;
+
+        v3 += SafeUtils.readIntLE(buf, off) * PRIME2;
+        v3 = rotateLeft(v3, 13);
+        v3 *= PRIME1;
+        off += 4;
+
+        v4 += SafeUtils.readIntLE(buf, off) * PRIME2;
+        v4 = rotateLeft(v4, 13);
+        v4 *= PRIME1;
+        off += 4;
+      } while (off <= limit);
+
+      h32 = rotateLeft(v1, 1) + rotateLeft(v2, 7) + rotateLeft(v3, 12) + rotateLeft(v4, 18);
+    } else {
+      h32 = seed + PRIME5;
+    }
+
+    h32 += len;
+
+    while (off <= end - 4) {
+      h32 += SafeUtils.readIntLE(buf, off) * PRIME3;
+      h32 = rotateLeft(h32, 17) * PRIME4;
+      off += 4;
+    }
+
+    while (off < end) {
+      h32 += (SafeUtils.readByte(buf, off) & 0xFF) * PRIME5;
+      h32 = rotateLeft(h32, 11) * PRIME1;
+      ++off;
+    }
+
+    h32 ^= h32 >>> 15;
+    h32 *= PRIME2;
+    h32 ^= h32 >>> 13;
+    h32 *= PRIME3;
+    h32 ^= h32 >>> 16;
+
+    return h32;
+  }
+
+
+
+}
+
